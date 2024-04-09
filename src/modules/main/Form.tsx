81bba4ba1b9f1aabc7f9b05e43ea264fdb40b0a1
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-04-09 21:57:21 +0700

    сверстана главная страница

--- src/modules/main/Form.tsx
+++ src/modules/main/Form.tsx
@@ -1,12 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 // @ts-ignore
-import { observer } from "mobx-react";
+import {observer} from "mobx-react";
+// @ts-ignore
+import logo from "../../assets/logo.svg"
+// @ts-ignore
+import card1 from "../../assets/card1.png"
+// @ts-ignore
+import card2 from "../../assets/card2.png"
+// @ts-ignore
+import card3 from "../../assets/card3.png"
+// @ts-ignore
+import card4 from "../../assets/card4.png"
 
 
 const Form = observer(() => {
 
-        return (
-            <h1>hello</h1>
+    return (
+        <body>
+            <header className="main__header">
+                <div className="main__logo">
+                    <img src={logo} alt="Логотип сайта"/>
+                </div>
+                <nav>
+                    <ul className="main__links-list">
+                        <li>
+                            <a href="#">
+                                <button className="main__link blue_button">Войти</button>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <button className="main__link grey_button">Создать аккаунт</button>
+                            </a>
+                        </li>
+                    </ul>
+
+                </nav>
+            </header>
+            <section className="main__cover">
+                <div className="overlay"></div>
+                <h1 className="main__cover__title">Путешествуй. Доберись вместе с Triptrack.</h1>
+            </section>
+            <section className="main__about">
+                <h2 className="main__about__title">Запланируй поездку вместе с TripTrack</h2>
+                <ul className="main__about__cards">
+                    <li>
+                        <div>
+                            <img src={card1} className="main__card-img"/>
+                            <h4 className="main__card-title">Храни список поездок</h4>
+                            <p className="main__card-text">Add multiple stops, edit your route, and get turn-by-turn directions.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src={card2}  className="main__card-img"/>
+                            <h4 className="main__card-title">Планируй поездки</h4>
+                            <p className="main__card-text">Book a flight, ride, or rental car for your trip.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src={card3}  className="main__card-img"/>
+                            <h4 className="main__card-title">Поделись воспоминаниями</h4>
+                            <p className="main__card-text">Find and book a hotel, vacation rental, or other accommodations.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src={card4}  className="main__card-img"/>
+                            <h4 className="main__card-title">Построй маршрут</h4>
+                            <p className="main__card-text">Get directions and show routes on a map.</p>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+        </body>
 
     );
 });
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import '../styles/global.css';
+import '../styles/style.css'
 import React from "react";
 import {Main} from "../modules/main/Main";
 
