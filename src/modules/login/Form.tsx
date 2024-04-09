77469ed35a23b55b9bd4fceeb8b634e723b0487c
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-04-10 00:12:15 +0700

    добавлена обработка данных

--- src/modules/login/Form.tsx
+++ src/modules/login/Form.tsx
@@ -3,10 +3,54 @@ import React, {useEffect, useState} from "react";
 import {observer} from "mobx-react";
 // @ts-ignore
 import logo from "../../assets/logo.svg";
+import validate from "./LoginFormValidationRules";
+import AuthService from "../../services/AuthService";
+import {authStore} from "../../store/AuthStore";
 
 
 
 const Form = observer(() => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>, setStateFunction: {
+        (value: React.SetStateAction<string>): void;
+        (value: React.SetStateAction<string>): void;
+        (value: React.SetStateAction<string>): void;
+        (value: React.SetStateAction<string>): void;
+        (value: React.SetStateAction<string>): void;
+        (arg0: any): void;
+    }) => {
+        const inputValue = event.target.value;
+        setStateFunction(inputValue);
+    };
+
+    const submit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        setEmailError(validate(email, password).email);
+        setPasswordError(validate(email, password).password);
+
+        if (emailError === '' && passwordError === '') {
+            AuthService.login(email, password)
+                .then(response => {
+
+                    authStore.login();
+                    localStorage.setItem('accessToken', response.data.accessToken);
+                    localStorage.setItem('refreshToken', response.data.refreshToken);
+
+                    window.location.assign('http://localhost:3000/settings');
+
+                })
+                .catch(error => {
+                    console.error('Ошибка при получении данных:', error);
+                    console.log(authStore.isAuthenticated)
+                })
+        }
+    }
 
     return (
         <body>
@@ -21,11 +65,21 @@ const Form = observer(() => {
                     <ul className="login__input-list">
                         <li>
                             <h3 className="login__input__title">Имя пользователя</h3>
-                            <input type="text" placeholder="Имя пользователя" className="login__input"/>
+                            <input type="text" placeholder="Имя пользователя" className="login__input"
+                                   onChange={ (event) => handleChange(event, setEmail) }
+                            />
+                            {emailError && (
+                                <p className="wrong_data">{ emailError }</p>
+                            )}
                         </li>
                         <li>
                             <h3 className="login__input__title">Пароль</h3>
-                            <input type="password" placeholder="Пароль" className="login__input"/>
+                            <input type="password" placeholder="Пароль" className="login__input"
+                                   onChange={ (event) => handleChange(event, setPassword)}
+                            />
+                            {passwordError && (
+                                <p className="wrong_data">{passwordError}</p>
+                            )}
                         </li>
                     </ul>
                 </div>
@@ -33,7 +87,7 @@ const Form = observer(() => {
                     <a className="grey_link">
                         <span>Еще нет аккаунта?</span>
                     </a>
-                    <button className="main__link blue_button">Войти</button>
+                    <button className="main__link blue_button" onClick={ submit }>Войти</button>
                 </div>
             </main>
         </body>
