6129d72e2183447e1f0dd8c23c8be1070a4f804d
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-06-08 19:26:31 +0700

    получение информации о пользователе с бэка

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,1 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
+
+const UserService = {
+    getUserInfo: async (personalKey: string) => {
+        try {
+            const response = await axios.get(`http://192.168.43.242:8080/triptrack/myinfo?personal_key=${personalKey}`);
+            const data = response.data;
+            const loginMatch = data.match(/login:(\w+)/);
+            if (loginMatch) {
+                return loginMatch[1];
+            }
+            throw new Error("Login not found in response");
+        } catch (error) {
+            console.error("Error fetching user info:", error);
+            throw error;
+        }
+    }
+};
+
+export default UserService;
